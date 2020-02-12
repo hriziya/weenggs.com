2d3866ed37d8f6ce564a56f70de53415431ae5bb
Hitesh Riziya <hriziya@gmail.com>
2020-02-12 11:01:53 +0530

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -9,10 +9,10 @@ module.exports = {
   socialLinks: {
     twitter: 'http://www.twitter.com/weenggs',
     github: 'https://github.com/weenggs',
-    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122',
+    linkedin: 'https://linkedin.com/company/weenggs-technology',
     instagram: 'https://instagram.com/weenggs',
-    youtube: 'https://www.youtube.com/user/weenggs',
-    google: 'https://plus.google.com/u/0/+weenggs',
+    youtube: 'https://www.youtube.com/channel/UCwVVv_NLlrzyamke_T0wSQQ/',
+    google: 'https://google.com?q=weenggs+technology',
   },
   googleAnalyticsID: 'UA-000000-0',
   themeColor: '#00c6ff',
